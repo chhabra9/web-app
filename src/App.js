7011cde112f9eda29076c58b308a032c6d86be28
Achint Singh Chhabra <71842622+chhabra9@users.noreply.github.com>
2023-09-16 00:08:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn angular
         </a>
       </header>
     </div>
